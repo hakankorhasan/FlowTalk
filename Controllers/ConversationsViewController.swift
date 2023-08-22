0e47d039d23a4a33bdcf01128f72b601996928d8
Hakan Körhasan <hakankorhasann@gmail.com>
2023-08-22 15:20:17 +0300

    Initial commit

---
+++ Controllers/ConversationsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlowTalk
+//
+//  Created by Hakan Körhasan on 22.08.2023.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Controllers/VideoPlayerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  VideoPlayerViewController.swift
+//  FlowTalk
+//
+//  Created by Hakan Körhasan on 22.08.2023.
+//
+
+import UIKit
+
+class VideoPlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
